3d5c67525dfe6d87832eedd5378599eb0fed102e
meashishpatel <ashishpatel3009@gmail.com>
2023-12-22 16:31:45 +0530

    Array.isArray,Array.from("Ashish")), Array.of(score1, score2, score3)

---
+++ 02_basics/02_array.js
@@ -0,0 +1,29 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"];
+const dc_heros = ["superman", "flash", "batman"];
+
+marvel_heros.push(dc_heros);
+
+// console.log(marvel_heros); // >>[ 'thor', 'Ironman', 'spiderman', [ 'superman', 'flash', 'batman' ] ]
+// console.log(marvel_heros[3][1]); //>> flash
+
+const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);  // >>[ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const all_new_heros = [...marvel_heros, ...dc_heros]; //Spread Operation - Works similar to conconate operator
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+console.log(real_another_array);
+
+console.log(Array.isArray("Ashish")); // returns boolean value
+console.log(Array.from("Ashish")); // Converts string into array
+console.log(Array.from({ name: "Ashish" })); // interesting
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // Instead of from you can use of also
