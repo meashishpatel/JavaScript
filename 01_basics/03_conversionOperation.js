3f0e450fcff51bf7a9f4eea8bd9dc4f387bb58de
meashishpatel <ashishpatel3009@gmail.com>
2023-12-22 12:43:07 +0530

    Conversions and operations completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,40 @@ let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+// ************************** Operations ***************************************
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 3);
+// console.log(2 ** 3);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " ashish";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + "2" + "2");
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2; // bad practices
+let gameCounter = 100;
+++gameCounter; // prefix operator
+gameCounter++; // postfix operator
+console.log(gameCounter);
