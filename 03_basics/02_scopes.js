accce7b1316870ca937e08a3f02ae6e9bab45445
meashishpatel <ashishpatel3009@gmail.com>
2023-12-23 11:28:05 +0530

    Child can access Parent's variables

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -7,3 +7,44 @@ if (true) {
 // console.log(a);
 // console.log(b);
 console.log(c);
+
+// In nested functions child function can access parent's variables
+function one() {
+  const username = "Ashish";
+
+  function two() {
+    const website = "Youtube";
+    console.log(username); // This will not throw error
+  }
+  //   console.log(website); // This will throw error
+  two();
+}
+
+one();
+
+if (true) {
+  const username = "Ashish";
+  if (username == "Ashish") {
+    const website = " youtube";
+    console.log(username + website);
+  }
+  //   console.log(website); // Parent's can't accesss child's variables
+}
+
+// console.log(username)// Error
+
+// ++++++++++++++++++++++++ interesting+++++++++++++++++++++++
+console.log(addone(5)); // No error
+function addone(num) {
+  return num + 1;
+}
+
+console.log(addone(5)); // No error
+
+// console.log(addTwo(5)); // Error
+const addTwo = function (num) {
+  // This is not function this is a expression
+  return num + 2;
+};
+
+console.log(addTwo(5)); // No error
